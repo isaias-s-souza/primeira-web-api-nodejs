1f19a71f19f85ee43c026665edd091911f1e1bb5
Isaias Souza <isaiassouza@brasilcard.net>
2022-02-23 13:44:27 -0300

    Finalizando primeiras rotas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const http = require('http')
 const PORT = 3000
 const DEFAULT_HEADER = {'Content-Type': 'application/json'}
+const HeroFactory = require('./factories/heroFactory')
+const heroService = HeroFactory.generateInstance()
 
 const routes = {
     '/heroes:get': async (request, response) => {
         const { id } = request.queryString
-        console.log({ id })
+        const heroes = await heroService.find(id)
+        response.write(JSON.stringify({ results: heroes }))
+
         return response.end()
     },
     default: (request, response) => {
-        response.write('Hello!')
+        response.write('Hello!') 
         response.end()
     }
 }
@@ -23,7 +27,8 @@ const handler = (request, response) => {
 
     response.writeHead(200, DEFAULT_HEADER)
 
-    response.end()
+    const chosen = routes[key] || routes.default
+    return chosen(request, response)
 }
 
 http.createServer(handler)
