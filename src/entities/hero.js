b7b408c87bb1160712b2dd7e4881b11199363fbe
Isaias Souza <isaiassouza@brasilcard.net>
2022-02-24 16:59:27 -0300

    Começando os comentários de todas partes do código que convém, para melhor compreensão.

--- src/entities/hero.js
+++ src/entities/hero.js
@@ -1,6 +1,9 @@
+// Definição classe Hero
 class Hero {
     constructor ({ id, name, age, power})
     {
+        // Id é definido por um numero randomico concatenado do number 
+        // que representa o instante do instanciamento
         this.id = Math.floor(Math.random() * 100) + Date.now()
         this.name = name
         this.age = age
@@ -8,11 +11,14 @@ class Hero {
     }
 
     isValid(){
+        // Object.getOwnPropertyNames(): Obtém um array com os nomes das 
+        // propriedades do objeto passado como parâmetro 
         const propertyNames = Object.getOwnPropertyNames(this)
         const amountInvalid = propertyNames
+            // .map aqui retorna um array que dado o array function passado é verificado 
+            // se existe e  TODO CONTINUAR COMENTANDO
             .map(property => (!!this[property]) ? null : `${property} is missing!`)
             .filter(item=> !!item)
-        
         return {
             valid: amountInvalid.length === 0,
             error: amountInvalid
